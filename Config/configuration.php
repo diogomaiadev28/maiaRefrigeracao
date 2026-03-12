d9dd41a7848587f7a3c56dfca678fc2ff5eebcf9
DiogoMaiaSESI <diogomaia.sesiba@gmail.com>
2026-03-12 19:23:06 -0300

    fix: Alteração do banco de dados local para o banco real.

--- Config/configuration.php
+++ Config/configuration.php
@@ -1,8 +1,8 @@
 <?php
 // CONFIGURAÇÃO DO BANCO DE DADOS
-define("DB_NAME","maiarefrigeracao");
+define("DB_NAME","u355873498_maiarefrigerac");
 define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASSWORD",'');
+define("DB_USER","u355873498_maia");
+define("DB_PASSWORD",'#Microcamp2016');
 define("DB_PORT","3306");
 ?>
